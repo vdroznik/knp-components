6bc347b1a1152cc7ee30de3be4df853190a2fa33
Vladimir Droznik <vladimir.droznik@gmail.com>
2013-08-15 22:00:28 +0300

    Use native count walker
    Doctrine count walker generates SELECT FROM SELECT FROM SELECT unoptimal query for big data sets

--- src/Knp/Component/Pager/Event/Subscriber/Paginate/Doctrine/ORM/QuerySubscriber.php
+++ src/Knp/Component/Pager/Event/Subscriber/Paginate/Doctrine/ORM/QuerySubscriber.php
@@ -25,14 +25,8 @@ class QuerySubscriber implements EventSubscriberInterface
     {
         if ($event->target instanceof Query) {
             // process count
-            $useDoctrineWalkers = false;
+            $useDoctrineWalkers = true;
             $useDoctrineOutputWalker = false;
-            if (version_compare(\Doctrine\ORM\Version::VERSION, '2.3.0', '>=')) {
-                $useDoctrineWalkers = true;
-                $useDoctrineOutputWalker = true;
-            } else if (version_compare(\Doctrine\ORM\Version::VERSION, '2.2.0', '>=')) {
-                $useDoctrineWalkers = true;
-            }
             if (($count = $event->target->getHint(self::HINT_COUNT)) !== false) {
                 $event->count = intval($count);
             } else {
